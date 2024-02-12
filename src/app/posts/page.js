6701d88d25445c4bcb6fbc5d29c73118ebe62e7b
salman-farcy <salmanfarcy253@gmail.com>
2024-02-12 20:47:40 +0600

    cache . force-cache

---
+++ src/app/posts/page.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const page = async () => {
+
+     const res = await fetch("http://localhost:5000/posts", {
+          cache: "force-cache",
+     });
+     const posts = await res.json();
+
+     // console.log(posts);
+
+
+     return (
+          <div>
+               <h1>Total Post: {posts.length} </h1>
+               <div className="flex gap-3">
+                    {
+                         posts.map(post => <div key={post.id} className="card flex  bg-gray-400 shadow-xl">
+                              <div className="card-body">
+                                   <h2 className="card-title">{post.title}</h2>
+                                   <p>{post.description}</p>
+                                   <div className="card-actions justify-end">
+                                       <p>{post.likes_count}</p>
+                                   </div>
+                              </div>
+                         </div>)
+                    }
+               </div>
+          </div>
+     );
+};
+
+export default page;
