f5b19bbc764a0e3d6125829f2243dc9643137dc6
salman-farcy <salmanfarcy253@gmail.com>
2024-02-13 18:30:37 +0600

    Placement of phased data step by step

--- src/components/Userss/Userss.js
+++ src/components/Userss/Userss.js
@@ -1,4 +1,5 @@
 "use client"
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
 const Userss = () => {
@@ -12,6 +13,25 @@ const Userss = () => {
      return (
           <div>
                <h1>Total Users: {users.length}</h1>
+
+               {
+                    users.map(user => <div key={user.id} className="card flex  bg-gray-400 shadow-xl">
+                    <div className="card-body">
+                         <h2 className="card-title">{user.name}</h2>
+                         <p>User Name: {user.username}</p>
+                         <p>Email :{user.email}</p>
+                         <p>Street :{user.address.street}</p>
+                         <p>Suite :{user.address.suite}</p>
+                         <p>City :{user.address.city}</p>
+                         <p>Zipcode: {user.address.zipcode}</p>
+                         {/* <div className="card-actions justify-end">
+                              <Link href={`/posts/${post.id}`}>
+                                   <button className="btn btn-primary">See More</button>
+                              </Link>
+                         </div> */}
+                    </div>
+               </div>)
+               }
           </div>
      );
 };
